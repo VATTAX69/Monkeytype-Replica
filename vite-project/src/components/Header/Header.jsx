1188c67b5820c5ea7eddb3bcdabe57b54631c41b
Vatshal Deru <vatshal101@gmail.com>
2024-06-19 18:41:42 +0100

    first commit

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+// import KeyboardIcon from "../../assets/KeyBoardIcon";
+import './Header.css'
+import KeyboardIcon from "../icons/KeyboardIcon/KeyboardIcon";
+import CrownRankingIcon from "../icons/CrownRankingIcon/CrownRankingIcon";
+import AboutIcon from "../icons/AboutIcon/AboutIcon";
+import SettingsIcon from "../icons/SettingsIcon/SettingsIcon";
+import BellIcon from "../icons/BellIcon/BellIcon";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="websiteLogo">
+                <div className="logo">
+                    <img src="./src/assets/monkey-type-logo.png" alt="monkey type logo" />
+                </div>
+                <div className="name">
+                    <p>monkeytype</p>
+                </div>
+            </div>
+            <nav>
+                <div className="typingConfigurations">
+                    <KeyboardIcon color = '#636768'/>
+                    <CrownRankingIcon color = '#636768'/>
+                    <AboutIcon color = '#636768'/>
+                    <SettingsIcon color = '#636768'/>
+                </div>
+                <div className="userConfiguration">
+                    <BellIcon color='#636768' />
+                </div>
+            </nav>
+        </header>
+    );
+}
